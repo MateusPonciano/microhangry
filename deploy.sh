8376c7a9db0c13905b6ffedf13a51cbb7be6e7bd
mmps <mmps@cin.ufpe.br>
2019-06-25 22:07:11 +0000

    teste heroku release

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
+cat >~/.netrc <<EOF
+machine api.heroku.com
+    login $HEROKU_EMAIL
+    password $HEROKU_TOKEN
+machine git.heroku.com
+    login $HEROKU_EMAIL
+    password $HEROKU_TOKEN
+EOF
+
 docker login -u _ -p "$HEROKU_TOKEN"  registry.heroku.com
 docker tag microhangry_core-api registry.heroku.com/micro-teste/web
 docker push registry.heroku.com/micro-teste/web
 docker run registry.heroku.com/micro-teste/web npm run typeorm -- migration:run
-heroku container:release web -a micro-teste --password $HEROKU_TOKEN --username
+heroku container:release web -a micro-teste
