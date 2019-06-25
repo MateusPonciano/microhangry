fdb4254cffa6b81ec639e836fa7751a3f1da3527
mmps <mmps@cin.ufpe.br>
2019-06-25 19:58:18 +0000

    teste deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+docker login -u _ -p "$HEROKU_TOKEN"  registry.heroku.com
+docker tag microhangry_core-api registry.heroku.com/micro-teste/web
+docker push registry.heroku.com/micro-teste/web
+docker run registry.heroku.com/micro-teste/web npm run typeorm -- migration:run
+heroku container:release
